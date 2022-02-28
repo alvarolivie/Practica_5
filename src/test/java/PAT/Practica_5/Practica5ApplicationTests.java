5172ecd831235343f2439a99651755212e0a5edc
alvarolivie <alvarolivie@gmail.com>
2022-02-28 19:41:47 +0100

    prac 5

---
+++ src/test/java/PAT/Practica_5/Practica5ApplicationTests.java
@@ -0,0 +1,13 @@
+package PAT.Practica_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
